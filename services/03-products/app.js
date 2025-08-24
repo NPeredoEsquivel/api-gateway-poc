0bae431fa1dae96b84e15e55e39a36e33b408fdf
Nicolas Peredo Esquivel <nperedoesquivel@gmail.com>
2025-08-24 12:50:41 -0400

    wip

---
+++ services/03-products/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/products', (req, res) => {
+  res.json({msg: 'List of products'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Product server is running on port ${PORT}`);
+});
+
